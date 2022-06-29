fab3f6e415e074b4a60f6f4e65b0e235fcb42d17
tingweichoo <54090185+tingweichoo@users.noreply.github.com>
2022-06-29 17:12:18 +0800

    Unit test for test result path

--- src/test/java/io/jenkins/plugins/eggplant/EggplantRunnerBuilderTest.java
+++ src/test/java/io/jenkins/plugins/eggplant/EggplantRunnerBuilderTest.java
@@ -1,11 +1,14 @@
-package io.jenkins.plugins.eggplant;
 
+package io.jenkins.plugins.eggplant;
+import static org.junit.Assert.assertEquals;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
+import hudson.util.FormValidation;
+import io.jenkins.plugins.eggplant.EggplantRunnerBuilder.DescriptorImpl;
 
 public class EggplantRunnerBuilderTest {
     
@@ -25,4 +28,49 @@ public class EggplantRunnerBuilderTest {
         FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
         jenkins.assertLogContains(String.format("Dry run of test configuration test-Config-Id against server http://localhost:8080"), build);
     }
+
+    @Test
+    public void testIsValidTestResultPath() throws Exception {
+        DescriptorImpl descriptorImpl=new DescriptorImpl();
+
+        FormValidation form = descriptorImpl.doCheckTestResultPath("");
+        assertEquals(FormValidation.Kind.OK, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("C:\\TestResultPath\\testResultFile.xml");
+        assertEquals(FormValidation.Kind.OK, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("\\TestResultPath\\testResultFile.xml");
+        assertEquals(FormValidation.Kind.OK, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("testResultFile.xml");
+        assertEquals(FormValidation.Kind.OK, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("testResultFile?.xml");
+        assertEquals(FormValidation.Kind.ERROR, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("testResultFile*.xml");
+        assertEquals(FormValidation.Kind.ERROR, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("testResultFile\".xml");
+        assertEquals(FormValidation.Kind.ERROR, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("testResultFile<.xml");
+        assertEquals(FormValidation.Kind.ERROR, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("testResultFile>.xml");
+        assertEquals(FormValidation.Kind.ERROR, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("testResultFile|.xml");
+        assertEquals(FormValidation.Kind.ERROR, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("                ");
+        assertEquals(FormValidation.Kind.ERROR, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("C:\\TestResultPath");
+        assertEquals(FormValidation.Kind.ERROR, form.kind);
+
+        form = descriptorImpl.doCheckTestResultPath("C:\\TestResultPath\\testResultFile.txt");
+        assertEquals(FormValidation.Kind.ERROR, form.kind);
+     }
+   
 }
